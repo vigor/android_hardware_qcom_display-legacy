2205ab4160b40c66af2973977d8026d7c0e90a7f
David Hays <dhays90@gmail.com>
2015-01-20 20:10:14 -0600

    display: Update paths and headers

--- Android.mk
+++ Android.mk
@@ -1,10 +1,15 @@
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
+ifeq ($(call my-dir),$(call project-path-for,qcom-display))
+
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils
+
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
 endif
-include $(call all-named-subdir-makefiles,$(display-hals))
+
+ifneq ($(filter msm8660,$(TARGET_BOARD_PLATFORM)),)
+    #This is for mako since it doesn't have the QCOM make functions
+    include $(call all-named-subdir-makefiles,$(display-hals))
 endif
+
 endif
